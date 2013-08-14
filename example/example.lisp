face6aa6f583ebe0c140900bffc05c6f8da1a401
TAHARA Yoshinori <read.eval.print@gmail.com>
2013-08-14 20:42:41 +0900

    example

--- example/example.lisp
+++ example/example.lisp
@@ -40,70 +40,53 @@
               do (html (:li (:a :href path path)))))))
 
 (defaction /env ()
-  (html
-    (:html
-      (:body
-          (:pre (with-output-to-string (*standard-output*)
-                  (describe *request*)))
-        (dump-env)))))
+  (default-template (:title "env の内味")
+    (:pre (with-output-to-string (*standard-output*)
+            (describe *request*)))
+    (dump-env)))
 
 (defaction /hello/@who ()
-  (html
-    (:html
-      (:body
-          (format nil "Hello ~a!" @who)))))
+  (default-template (:title "URL からのパラメータ")
+    (format nil "Hello ~a!" @who)
+    (:p (:a :href "/hello/あいう" "/hello/あいう"))))
 
 (defaction /room ()
-  (html
-    (:html
-      (:body
-          (:h1 "room")
-        (:pre
-            (with-output-to-string (*standard-output*) (room)))))))
+  (default-template (:title "room")
+    (:h1 "room")
+    (:pre
+        (with-output-to-string (*standard-output*) (room)))))
 
 (defaction /form/a (:method :get)
-  (html
-    (:html
-      (:head
-          (:meta :charset :utf-8))
-      (:body
-          (:form :action "/form/a" :method :post
-            (:input :type :text :name :a :value "あいう")
-            (:input :type :submit :value "サブミット"))))))
+  (default-template (:title "フォーム")
+    (:form :action "/form/a" :method :post
+      (:input :type :text :name :a :value "あいう")
+      (:input :type :submit :value "サブミット"))))
 
 (defaction /form/a (:method :post)
-  (html
-    (:html
-      (:body
-          (:p (local-time:now))
-        (:p "[" @a"]")
-        (dump-env)))))
+  (default-template (:title "フォームの先")
+    (:p (local-time:now))
+    (:p "[" @a "]")
+    (dump-env)))
 
 (defaction /form/file (:method :get)
-  (html
-    (:html
-      (:head
-          (:meta :charset :utf-8))
-      (:body
-          (:form :action "/form/file" :method :post :enctype "multipart/form-data"
-            (:input :type :text :name "a" :value "あいう")
-            (:input :type :file :name "file")
-            (:input :type :submit :value "サブミット"))))))
+  (default-template (:title "画像のアップロード")
+    (:form :action "/form/file" :method :post :enctype "multipart/form-data"
+      (:input :type :text :name "a" :value "あいう")
+      (:input :type :file :name "file")
+      (:input :type :submit :value "サブミット"))))
 
 (defaction /form/file (:method :post)
   (when @file
     (alexandria:copy-file (car @file) "/tmp/unpyo-scranch-upload-file"))
-  (html
-    (:html
-      (:body
-          (:p "[" @a "]")
-        (:pre @file)
-        (:pre (format nil "~s" (slot-value *request* 'unpyo::params)))
-        (:img :src "/form/file/uploaded")
-        (:ul
-            (maphash (lambda (key value)
-                       (html (:li (format nil "~a = ~a" key value))))
-                     (slot-value *request* 'env)))))))
+  (default-template (:title "画像のアップロード先")
+    (:p "[" @a "]")
+    (:pre @file)
+    (:pre (format nil "~s" (slot-value *request* 'unpyo::params)))
+    (:img :src "/form/file/uploaded")
+    (:ul
+        (maphash (lambda (key value)
+                   (html (:li (format nil "~a = ~a" key value))))
+                 (slot-value *request* 'env)))))
 
 (defaction /form/file/uploaded ()
   (write-sequence (alexandria:read-file-into-byte-vector "/tmp/unpyo-scranch-upload-file")
