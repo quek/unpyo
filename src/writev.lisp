9414623133ddd900bbd9efcc62605a4ef7ff2c25
TAHARA Yoshinori <read.eval.print@gmail.com>
2015-12-12 11:38:33 +0900

    iov-max

--- src/writev.lisp
+++ src/writev.lisp
@@ -1,29 +1,33 @@
 (in-package :unpyo)
 
+(defconstant +iov-max+ 1024)
+
 (cffi:defcstruct iovec
   ;; Starting address
   (iov-base :pointer)
   ;; Number of bytes to transfer
   (iov-len iolib.syscalls:size-t))
 
-(defun writev (fd vector)
+(declaim (inline %writev))
+(defun %writev (fd vector start end)
   (declare (optimize (speed 3) (safety 0) (debug 0))
            (type (simple-array * (*)) vector)
-           (type fixnum fd))
-  (labels ((call-with-pinned-objects (i fun)
-             (declare (type (function () t) fun))
-             (if (< i (length vector))
-                 (sb-sys:with-pinned-objects ((aref vector i))
-                   (call-with-pinned-objects (1+ i) fun))
-                 (funcall fun))))
-    (declare (inline call-with-pinned-objects))
-    (call-with-pinned-objects
-     0
-     (lambda ()
-       (let ((iovcnt (length vector)))
+           (type fixnum fd start end))
+  (let ((iovcnt (- end start)))
+    (labels ((call-with-pinned-objects (i fun)
+               (declare (type (function () t) fun))
+               (if (< i end)
+                   (sb-sys:with-pinned-objects ((aref vector i))
+                     (call-with-pinned-objects (1+ i) fun))
+                   (funcall fun))))
+      (declare (inline call-with-pinned-objects))
+      (call-with-pinned-objects
+       start
+       (lambda ()
          (cffi:with-foreign-object (iov '(:struct iovec) iovcnt)
            (loop for p = iov then (cffi:inc-pointer p (cffi:foreign-type-size '(:struct iovec)))
-                 for i across vector
+                 for index from start below end
+                 for i = (aref vector index)
                  do (cffi:with-foreign-slots ((iov-base iov-len) p (:struct iovec))
                       (setf iov-base (sb-sys:vector-sap i)
                             iov-len (length i))))
@@ -48,3 +52,15 @@
                                        (cffi:inc-pointer p write-size)
                                        (decf (the fixnum iov-len) write-size)
                                        (loop-finish)))))))))))))
+
+(defun writev (fd vector)
+  (declare (optimize (speed 3) (safety 0) (debug 0))
+           (type (simple-array * (*)) vector)
+           (type fixnum fd))
+  (loop with length fixnum = (length vector)
+        for start fixnum = 0 then end
+        for end fixnum  = (min (+ start +iov-max+) length)
+        while (< start length)
+        do (%writev fd vector start end)))
+
+
