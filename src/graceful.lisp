6c63aceb153626c6456714e1b12a7e31ea9ecb81
TAHARA Yoshinori <read.eval.print@gmail.com>
2017-08-06 18:23:06 +0900

    fork して execv

--- src/graceful.lisp
+++ src/graceful.lisp
@@ -13,30 +13,26 @@
 
 (defun sighup-handler (signal code context)
   (log:debug signal)
-  (let ((pid (sb-posix:posix-fork)))
-    (log:debug pid)
-    (if (zerop pid)
-        (let ((arg0 (car sb-ext:*posix-argv*)))
-          (log:debug sb-ext:*posix-argv*)
-          (cffi:with-foreign-strings ((path arg0))
-            (cffi:with-foreign-object (argv :pointer 2)
-              (setf (cffi:mem-aref argv :pointer 0) path
-                    (cffi:mem-aref argv :pointer 1) (cffi:null-pointer))
-              (sb-posix:setenv "UNPYO_FD"
-                               (princ-to-string
-                                (sb-bsd-sockets:socket-file-descriptor
-                                 (unpyo::server-socket *server*)))
-                               1)
-              (log:debug "before execv" arg0)
+  (sb-posix:setenv "UNPYO_FD"
+                   (princ-to-string
+                    (sb-bsd-sockets:socket-file-descriptor
+                     (unpyo::server-socket *server*)))
+                   1)
+  (cffi:with-foreign-strings ((path (car sb-ext:*posix-argv*)))
+    (cffi:with-foreign-object (argv :pointer 2)
+      (setf (cffi:mem-aref argv :pointer 0) path
+            (cffi:mem-aref argv :pointer 1) (cffi:null-pointer))
+      (let ((pid (sb-posix:posix-fork)))
+        (if (zerop pid)
+            (progn
               (execv path argv)
-              (log:debug "after execv... execv failed" arg0)
-              (sb-ext:exit :code 255))))
-        (setf *exit-function*
-              (lambda ()
-                (log:debug *exit-function*)
-                (if (functionp *old-sighup-handler*)
-                    (funcall *old-sighup-handler*
-                             signal code context)))))))
+              (log:debug "after execv... execv failed" sb-ext:*posix-argv*)
+              (sb-ext:exit :code 255))
+            (setf *exit-function*
+                  (lambda ()
+                    (if (functionp *old-sighup-handler*)
+                        (funcall *old-sighup-handler*
+                                 signal code context)))))))))
 
 (defun enable-graceful-restart ()
   (setf *old-sighup-handler* (sb-c::enable-interrupt sb-vm::sighup #'sighup-handler)))
