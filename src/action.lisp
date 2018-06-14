2052db72b62f0bf6235c8293dc100a22684ccb75
TAHARA Yoshinori <read.eval.print@gmail.com>
2018-06-14 09:11:50 +0900

    パラメータをデバッグログにはく

--- src/action.lisp
+++ src/action.lisp
@@ -46,6 +46,7 @@
          (defun ,name-method ()
            (let ((,start (local-time:now)))
              (log:info "start action")
+             (log:debug (request-params *request*))
              (unwind-protect
                   (with-@param ,@body)
                (multiple-value-bind (m n)
